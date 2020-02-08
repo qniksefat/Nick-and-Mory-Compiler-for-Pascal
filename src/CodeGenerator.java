85e30712eada5c1e5d52cd4a14b52ebcb5a57022
qniksefat <qniksefat@gmail.com>
2020-02-08 18:55:00 +0330

    switch case on CG

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -1,9 +1,14 @@
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class CodeGenerator {
     Scanner scanner; // This was my way of informing CG about Constant Values detected by Scanner, you can do whatever you like
 
     // Define any variables needed for code generation
+    HashMap<String, String> symbolTable = new HashMap<>();
+    // hashes two strings and you can push them!
+    Stack<String> semanticStack = new Stack<>();
 
     public CodeGenerator(Scanner scanner) {
         this.scanner = scanner;
@@ -12,12 +17,18 @@ public class CodeGenerator {
     public void Generate(String sem) {
         System.out.println(sem); // Just for debug
 
-        if (sem.equals("NoSem"))
-            return;
-        else if (sem.equals("@push_func")) {
-            System.out.println("hey!!!!!");
-            return;
+        switch (sem) {
+            case "@push_func": {
+                System.out.println("hey!!!!!");
+                return;
+            }
+//            break;
+            case "NoSem": {
+                return;
+            }
         }
+//        break;
+
         /*
          * else if (sem.equals("SemanticRoutine1"))
          * {
